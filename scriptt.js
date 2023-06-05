efb90e1b01620d029dc23a19d691472569538aa4
SingarajuNavya <92417152+SingarajuNavya@users.noreply.github.com>
2023-06-05 21:41:10 +0530

    performed first dom manipulation

---
+++ scriptt.js
@@ -0,0 +1,17 @@
+var data1={image:"imggg.jpeg",name: "Singaraju Navya",designation: "Intern"};
+var data2={image:"img1.jpeg",name:"Navya",designation:"Software Developer"};
+var isdata1=true;
+var displaydata;
+var changemypic=function()
+{
+    if(isdata1)
+    {
+        displaydata=data2;
+        isdata1=false;
+    }
+    else{
+        displaydata=data1;
+        data2=false;
+    }
+    document.getElementById("myimage").src=displaydata.image;
+}
