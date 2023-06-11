e2ad70cc9ba0f5cd27ee7386c493f5f45eb8d495
SingarajuNavya <92417152+SingarajuNavya@users.noreply.github.com>
2023-06-11 17:15:50 +0530

    js

--- weather.js
+++ weather.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
     
     searchBtn.addEventListener('click', function() {
       const city = document.getElementById('cityInput').value;
-      const apiUrl = `https://beingzero-mern-bootcamp.cyclic.app/weather-3https://api.openweathermap.org/data/2.5/weather?q=delhi&appid=eb7a2d4a5a9b284fe54b7bb547238443&units=metric`;
+      const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=delhi&appid=eb7a2d4a5a9b284fe54b7bb547238443&units=metric`;
   
       // Fetch weather data from the API
       fetch(apiUrl)
@@ -14,10 +14,12 @@ document.addEventListener('DOMContentLoaded', function() {
           document.getElementById('location').textContent = data.name;
           document.getElementById('temperature').textContent = `Temperature: ${data.main.temp}°C`;
           document.getElementById('description').textContent = `Description: ${data.weather[0].description}`;
+          document.getElementById('humidity').textContent = `Humidity: ${data.main.humidity}%`;
+          document.getElementById('wind-speed').textContent = `Wind Speed: ${data.wind.speed} m/s`;
+          // You can add more elements to display additional weather information as needed
         })
         .catch(error => {
           console.log('Error fetching weather data:', error);
         });
     });
-  });
-  
+});
