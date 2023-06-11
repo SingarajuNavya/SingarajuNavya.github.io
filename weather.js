8cffd11f93ee0ebb7fcde9a7f4551c70b2bab4ae
SingarajuNavya <92417152+SingarajuNavya@users.noreply.github.com>
2023-06-11 17:09:45 +0530

    js

--- weather.js
+++ weather.js
@@ -1,20 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
     const apiKey = 'YOUR_API_KEY';
-    const city = 'CITY_NAME';
-    const apiUrl = `https://beingzero-mern-bootcamp.cyclic.app/weather-3https://api.openweathermap.org/data/2.5/weather?q=delhi&appid=eb7a2d4a5a9b284fe54b7bb547238443&units=metric`;
+    const searchBtn = document.getElementById('searchBtn');
+    
+    searchBtn.addEventListener('click', function() {
+      const city = document.getElementById('cityInput').value;
+      const apiUrl = `https://beingzero-mern-bootcamp.cyclic.app/weather-3https://api.openweathermap.org/data/2.5/weather?q=delhi&appid=eb7a2d4a5a9b284fe54b7bb547238443&units=metric`;
   
-    // Fetch weather data from the API
-    fetch(apiUrl)
-      .then(response => response.json())
-      .then(data => {
-        // Update the HTML elements with weather information
-        document.getElementById('location').textContent = data.name;
-        document.getElementById('temperature').textContent = `Temperature: ${data.main.temp}°C`;
-        document.getElementById('description').textContent = `Description: ${data.weather[0].description}`;
-      })
-      .catch(error => {
-        console.log('Error fetching weather data:', error);
-      });
+      // Fetch weather data from the API
+      fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+          // Update the HTML elements with weather information
+          document.getElementById('location').textContent = data.name;
+          document.getElementById('temperature').textContent = `Temperature: ${data.main.temp}°C`;
+          document.getElementById('description').textContent = `Description: ${data.weather[0].description}`;
+        })
+        .catch(error => {
+          console.log('Error fetching weather data:', error);
+        });
+    });
   });
-  
   
